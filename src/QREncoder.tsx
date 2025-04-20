2e17ba573a8c4f0eaffd5c1c9aa4acd92aeb55b6
Steven X. Han <steven@stevenhan.net>
2025-04-20 19:48:42 +1000

    Performance tweaks??

--- src/QREncoder.tsx
+++ src/QREncoder.tsx
@@ -1,5 +1,7 @@
+import { faBomb, faHandFist } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
-import { Form } from "react-bootstrap";
+import { Alert, Form } from "react-bootstrap";
 import QRCode from "react-qr-code";
 
 const LMAX = 2953;
@@ -9,99 +11,130 @@ const HMAX = 1273;
 const CAMMAX = 1000;
 
 const SIZE_LIMITS = {
-    L: LMAX,
-    M: MMAX,
-    Q: QMAX,
-    H: HMAX,
+  L: LMAX,
+  M: MMAX,
+  Q: QMAX,
+  H: HMAX,
 };
 
 const ERROR_CORRECTION_LEVEL = "L";
 const BLOCK_SIZE = Math.min(SIZE_LIMITS[ERROR_CORRECTION_LEVEL], CAMMAX);
 
+const FPS = 20;
+
 const QREncoder = () => {
-    const [encodedRep, setEncodedRep] = useState<string>("");
+  const [encodedRep, setEncodedRep] = useState<string>("");
 
-    const [currentBlock, setCurrentBlock] = useState<string>("");
-    const [blockIndex, setBlockIndex] = useState<number>(0);
+  const [currentBlock, setCurrentBlock] = useState<string>("");
+  const [blockIndex, setBlockIndex] = useState<number>(0);
 
-    const convertFileToBase64 = (file: File): Promise<string> => {
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader();
+  const [showSizeError, setShowSizeError] = useState<boolean>(false);
+  const [showSizeWarning, setShowSizeWarning] = useState<boolean>(false);
 
-            reader.readAsDataURL(file);
-            reader.onload = () => {
-                const base64String = (reader.result as string).split(",")[1];
-                resolve(base64String);
-            };
-            reader.onerror = (error) => reject(error);
-        });
-    };
+  const convertFileToBase64 = (file: File): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
 
-    useEffect(() => {
-        setTimeout(() => {
-            if (!encodedRep || encodedRep === "") {
-                return;
-            } else if (blockIndex > encodedRep.length / BLOCK_SIZE) {
-                setBlockIndex(0);
-            } else {
-                setCurrentBlock(
-                    ` =!= ${blockIndex + 1} =!= ${Math.ceil(
-                        encodedRep.length / BLOCK_SIZE
-                    )} =!= ${encodedRep.slice(
-                        blockIndex * BLOCK_SIZE,
-                        blockIndex * BLOCK_SIZE + BLOCK_SIZE
-                    )}`
-                );
-                setBlockIndex(blockIndex + 1);
-            }
-        }, 130);
-    }, [blockIndex, currentBlock, encodedRep]);
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        const base64String = (reader.result as string).split(",")[1];
+        resolve(base64String);
+      };
+      reader.onerror = (error) => reject(error);
+    });
+  };
 
-    const handleFileChange = async (
-        event: React.ChangeEvent<HTMLInputElement>
-    ) => {
-        const file = event.target.files?.[0];
-        if (!file) return;
+  useEffect(() => {
+    if (showSizeError) return;
+    setTimeout(() => {
+      if (!encodedRep || encodedRep === "") {
+        return;
+      } else if (blockIndex > encodedRep.length / BLOCK_SIZE) {
+        setBlockIndex(0);
+      } else {
+        setCurrentBlock(
+          ` =!= ${blockIndex + 1} =!= ${Math.ceil(
+            encodedRep.length / BLOCK_SIZE
+          )} =!= ${encodedRep.slice(
+            blockIndex * BLOCK_SIZE,
+            blockIndex * BLOCK_SIZE + BLOCK_SIZE
+          )}`
+        );
+        setBlockIndex(blockIndex + 1);
+      }
+    }, 1000 / FPS);
+  }, [blockIndex, currentBlock, encodedRep, showSizeError]);
 
-        try {
-            const encoded = await convertFileToBase64(file);
-            setEncodedRep(`${file.name}////${encoded}`);
-        } catch (error) {
-            console.error("Error converting file to base64.", error);
-        }
-    };
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
 
-    return (
-        <Form.Group controlId="formFileLg" className="mb-3">
-            <Form.Control
-                className="mb-3"
-                type="file"
-                size="lg"
-                onChange={handleFileChange}
+    if (file.size > 5 * 1024 * 1024) {
+      setShowSizeError(true);
+      return;
+    } else {
+      setShowSizeError(false);
+    }
+
+    if (file.size > 3 * 1024 * 1024) {
+      setShowSizeWarning(true);
+    } else {
+      setShowSizeWarning(false);
+    }
+
+    try {
+      const encoded = await convertFileToBase64(file);
+      setEncodedRep(`${file.name}////${encoded}`);
+    } catch (error) {
+      console.error("Error converting file to base64.", error);
+    }
+  };
+
+  return (
+    <Form.Group controlId="formFileLg" className="mb-3">
+      <Form.Control
+        className="mb-3"
+        type="file"
+        size="lg"
+        onChange={handleFileChange}
+      />
+      {showSizeError && (
+        <Alert key="size-error" variant="danger">
+          <FontAwesomeIcon icon={faBomb} /> The size of the file you have
+          selected is too large. The maximum supported size is 5MB.
+        </Alert>
+      )}
+      {showSizeWarning && (
+        <Alert key="size-error" variant="info">
+          <FontAwesomeIcon icon={faHandFist} /> Get ready to be here for a
+          while...
+        </Alert>
+      )}
+      {!showSizeError && encodedRep && (
+        <div>
+          <div style={{ width: "100%" }}>
+            <QRCode
+              className="mb-2"
+              level={ERROR_CORRECTION_LEVEL}
+              style={{ width: "100%", height: "auto" }}
+              // size={512}
+              value={currentBlock}
             />
-            {encodedRep && (
-                <div>
-                    <div style={{ width: "100%" }}>
-                        <QRCode
-                            className="mb-2"
-                            level={ERROR_CORRECTION_LEVEL}
-                            style={{ width: "100%", height: "auto" }}
-                            // size={512}
-                            value={currentBlock}
-                        />
-                    </div>
-                    <p
-                        style={{
-                            textAlign: "center",
-                            color: "grey",
-                        }}
-                    >
-                        {blockIndex} out of {Math.floor(encodedRep.length / BLOCK_SIZE) + 1}
-                    </p>
-                </div>
-            )}
-        </Form.Group>
-    );
+          </div>
+          <p
+            style={{
+              textAlign: "center",
+              color: "grey",
+            }}
+          >
+            {blockIndex} out of {Math.floor(encodedRep.length / BLOCK_SIZE) + 1}
+          </p>
+        </div>
+      )}
+    </Form.Group>
+  );
 };
 
 export default QREncoder;
--- src/QrCodeScanner.tsx
+++ src/QrCodeScanner.tsx
@@ -1,60 +1,60 @@
 import {
-    Html5QrcodeResult,
-    Html5QrcodeScanner,
-    Html5QrcodeScanType,
-    Html5QrcodeSupportedFormats,
+  Html5QrcodeResult,
+  Html5QrcodeScanner,
+  Html5QrcodeScanType,
+  Html5QrcodeSupportedFormats,
 } from "html5-qrcode";
 import { useEffect, useRef } from "react";
 
 interface QrcodeScannerProps {
-    onScanSuccess: (decodedText: string) => void;
+  onScanSuccess: (decodedText: string) => void;
 }
 
 const QrCodeScanner = ({ onScanSuccess }: QrcodeScannerProps) => {
-    const scannerRef = useRef<Html5QrcodeScanner | null>(null);
+  const scannerRef = useRef<Html5QrcodeScanner | null>(null);
 
-    useEffect(() => {
-        // Configuration for the scanner
-        const config = {
-            fps: 30,
-            // qrbox: { width: 500, height: 500 },
-            formatsToSupport: [
-                Html5QrcodeSupportedFormats.QR_CODE,
-            ] as Html5QrcodeSupportedFormats[],
-            rememberLastUsedCamera: true,
-            supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
-        };
+  useEffect(() => {
+    // Configuration for the scanner
+    const config = {
+      fps: 120,
+      // qrbox: { width: 500, height: 500 },
+      formatsToSupport: [
+        Html5QrcodeSupportedFormats.QR_CODE,
+      ] as Html5QrcodeSupportedFormats[],
+      rememberLastUsedCamera: true,
+      supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
+    };
 
-        // Initialize the scanner
-        const scanner = new Html5QrcodeScanner("qr-reader", config, true);
-        scannerRef.current = scanner;
+    // Initialize the scanner
+    const scanner = new Html5QrcodeScanner("qr-reader", config, true);
+    scannerRef.current = scanner;
 
-        const handleScanSuccess = (
-            decodedText: string,
-            result: Html5QrcodeResult
-        ) => {
-            onScanSuccess(decodedText);
-        };
+    const handleScanSuccess = (
+      decodedText: string,
+      result: Html5QrcodeResult
+    ) => {
+      onScanSuccess(decodedText);
+    };
 
-        const onScanFailure = (error: string) => { };
+    const onScanFailure = (error: string) => {};
 
-        scanner.render(handleScanSuccess, onScanFailure);
+    scanner.render(handleScanSuccess, onScanFailure);
 
-        // Cleanup function
-        return () => {
-            if (scannerRef.current) {
-                scannerRef.current.clear().catch((error) => {
-                    console.error("Failed to clear html5QrcodeScanner.", error);
-                });
-            }
-        };
-    }, []);
+    // Cleanup function
+    return () => {
+      if (scannerRef.current) {
+        scannerRef.current.clear().catch((error) => {
+          console.error("Failed to clear html5QrcodeScanner.", error);
+        });
+      }
+    };
+  }, []);
 
-    return (
-        <div>
-            <div id="qr-reader"></div>
-        </div>
-    );
+  return (
+    <div>
+      <div id="qr-reader"></div>
+    </div>
+  );
 };
 
 export default QrCodeScanner;
