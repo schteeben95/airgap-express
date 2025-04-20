8f2c5d1718cf4eadeaa33ad8c7420df5e5c3a721
Steven X. Han <steven@stevenhan.net>
2025-04-20 18:55:09 +1000

    Styling & formatting

--- src/QrCodeScanner.tsx
+++ src/QrCodeScanner.tsx
@@ -1,8 +1,13 @@
-import { Html5QrcodeResult, Html5QrcodeScanner, Html5QrcodeScanType, Html5QrcodeSupportedFormats } from "html5-qrcode";
+import {
+    Html5QrcodeResult,
+    Html5QrcodeScanner,
+    Html5QrcodeScanType,
+    Html5QrcodeSupportedFormats,
+} from "html5-qrcode";
 import { useEffect, useRef } from "react";
 
 interface QrcodeScannerProps {
-    onScanSuccess: (decodedText: string) => void
+    onScanSuccess: (decodedText: string) => void;
 }
 
 const QrCodeScanner = ({ onScanSuccess }: QrcodeScannerProps) => {
@@ -13,30 +18,33 @@ const QrCodeScanner = ({ onScanSuccess }: QrcodeScannerProps) => {
         const config = {
             fps: 30,
             // qrbox: { width: 500, height: 500 },
-            formatsToSupport: [Html5QrcodeSupportedFormats.QR_CODE] as Html5QrcodeSupportedFormats[],
+            formatsToSupport: [
+                Html5QrcodeSupportedFormats.QR_CODE,
+            ] as Html5QrcodeSupportedFormats[],
             rememberLastUsedCamera: true,
             supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
         };
 
         // Initialize the scanner
-        const scanner = new Html5QrcodeScanner('qr-reader', config, false);
+        const scanner = new Html5QrcodeScanner("qr-reader", config, false);
         scannerRef.current = scanner;
 
-        const handleScanSuccess = (decodedText: string, result: Html5QrcodeResult) => {
-            onScanSuccess(decodedText)
+        const handleScanSuccess = (
+            decodedText: string,
+            result: Html5QrcodeResult
+        ) => {
+            onScanSuccess(decodedText);
             // console.log(`${decodedText.split(" =!= ")[1]} out of ${decodedText.split(" =!= ")[2]}`)
         };
 
-        const onScanFailure = (error: string) => {
-
-        };
+        const onScanFailure = (error: string) => { };
 
         scanner.render(handleScanSuccess, onScanFailure);
 
         // Cleanup function
         return () => {
             if (scannerRef.current) {
-                scannerRef.current.clear().catch(error => {
+                scannerRef.current.clear().catch((error) => {
                     console.error("Failed to clear html5QrcodeScanner.", error);
                 });
             }
@@ -44,8 +52,8 @@ const QrCodeScanner = ({ onScanSuccess }: QrcodeScannerProps) => {
     }, []);
 
     return (
-        <div style={{ width: '500px', margin: '0 auto' }}>
-            <div id="qr-reader" style={{ width: '100%' }}></div>
+        <div>
+            <div id="qr-reader"></div>
         </div>
     );
 };
